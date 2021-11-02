8c9f3441a42a9f9234594bc9cc67b3adb081f77b
reruo321 <48712088+reruo321@users.noreply.github.com>
2021-11-02 19:02:50 +0900

    Add files via upload

---
+++ 018-DeepCopy-Operator-Equal/main.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Person {
+	char* name;
+	int age;
+public:
+	Person(const char* myname, int myage) {
+		int len = strlen(myname) + 1;
+		name = new char[len];
+		strcpy(name, myname);
+		age = myage;
+	}
+
+	/* TODO: Deep Copy for Operator= */
+
+	Person& operator=(const Person& p) {
+		name = new char[strlen(p.name) + 1];
+		strcpy(name, p.name);
+		age = p.age;
+		return *this;
+	}
+
+	void ShowPersonInfo() const {
+		cout << "이름: " << name << endl;
+		cout << "나이: " << age << endl;
+	}
+	~Person() {
+		delete[]name;
+		cout << "called destructor!" << endl;
+	}
+};
+
+int main() {
+	Person man1("Lee dong woo", 29);
+	Person man2("Yoon ji yul", 22);
+	man2 = man1;
+	man1.ShowPersonInfo();
+	man2.ShowPersonInfo();
+
+	return 0;
+}
