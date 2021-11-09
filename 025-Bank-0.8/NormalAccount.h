a23d01db058cb808bf05efded71939fe8801dd59
reruo321 <48712088+reruo321@users.noreply.github.com>
2021-11-09 17:46:29 +0900

    Update NormalAccount.h

--- 025-Bank-0.8/NormalAccount.h
+++ 025-Bank-0.8/NormalAccount.h
@@ -20,7 +20,7 @@ public:
 
     void UserInfo() const {
         Account::UserInfo();
-        cout << "ÀÌÀÚÀ²: " << interest << endl;
+        cout << "ì´ìžìœ¨: " << interest << endl;
     }
 
     int GetInterest() const {
